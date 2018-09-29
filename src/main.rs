1b0da655aebd1ce1b0b7528dbce12a86ad4e5941
Martin Barksten <martin.barksten@gmail.com>
2018-09-29 17:09:54 +0200

    Allow files to be removed

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ extern crate pathdiff;
 extern crate failure;
 
 use failure::Error;
-use std::path::Path;
 use getopts::Options;
 use git2::Repository;
+use pathdiff::diff_paths;
 use std::env;
 use std::fs;
-use pathdiff::diff_paths;
+use std::path::{Path, PathBuf};
 
-fn print_usage(program: &str, opts: Options) {
-  let brief = format!("Usage: {} [options] target", program);
+fn print_usage(program: &str, opts: &Options) {
+  let brief = format!("Usage: {} [options] command", program);
   print!("{}", opts.usage(&brief));
 }
 
@@ -30,7 +30,7 @@ fn main() {
   };
 
   if matches.opt_present("h") {
-    print_usage(&program, opts);
+    print_usage(&program, &opts);
     return;
   }
 
@@ -40,41 +40,61 @@ fn main() {
 
   let res = match matches.free[0].clone().as_ref() {
     "init" => init_repo(),
-    "add" => add_file(matches.free[1].clone()),
+    "add" => index_fn(&matches.free[1].clone(), add_file),
+    "remove" => index_fn(&matches.free[1].clone(), remove_file),
     cmd => Err(format_err!("invalid command: {}", cmd)),
   };
 
   match res {
-    Ok(_) => print!("ok!"),
+    Ok(_) => (),
     Err(e) => print!("{:?}", e),
   }
 }
 
 fn init_repo() -> Result<(), Error> {
-  let home = dirs::home_dir().expect("unable to find home directory");
+  let home = dirs::home_dir().ok_or_else(|| format_err!("unable to get home directory"))?;
   let dots = home.join(".dots");
   fs::create_dir_all(&dots)?;
 
-  let mut config = Repository::init_bare(dots)?.config()?;
+  let mut config = Repository::init_bare(&dots)?.config()?;
   config.set_str("status.showUntrackedFiles", "no")?;
 
+  print!("initialised new bare dotfiles-reop at {:?}", dots);
   Ok(())
 }
 
-fn add_file(file: String) -> Result<(), Error> {
-  let home = dirs::home_dir().expect("unable to find home directory");
-  let dots = home.join(".dots");
-  let path = Path::new(&file);
-
-  let rel = diff_paths(path, home.as_path()).unwrap_or(path.to_path_buf());
-
-  let repo = Repository::open_bare(dots)?;
-  repo.set_workdir(home.as_path(), false)?;
+fn index_fn<F>(file: &str, mut operation: F) -> Result<(), Error>
+where
+  F: FnMut(&mut git2::Index, &PathBuf) -> Result<(), git2::Error>,
+{
+  let home = dirs::home_dir().ok_or_else(|| format_err!("unable to get home directory"))?;
+  let repo = get_dots()?;
   let mut index = repo.index()?;
 
-  index.add_path(&rel)?; 
+  let path = Path::new(&file);
+  let rel = diff_paths(path, home.as_path()).unwrap_or_else(|| path.to_path_buf());
+
+  operation(&mut index, &rel)?;
 
   index.write()?;
 
   Ok(())
+}
+
+fn add_file(index: &mut git2::Index, path: &PathBuf) -> Result<(), git2::Error> {
+  index.add_path(path)
+}
+
+fn remove_file(index: &mut git2::Index, path: &PathBuf) -> Result<(), git2::Error> {
+  index.remove_path(path)
+}
+
+fn get_dots() -> Result<git2::Repository, Error> {
+  let home = dirs::home_dir().ok_or_else(|| format_err!("unable to get home directory"))?;
+  let dots = home.join(".dots");
+
+  let repo = Repository::open_bare(dots)?;
+  repo.set_workdir(home.as_path(), false)?;
+
+  Ok(repo)
 }
